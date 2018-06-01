74c118ff0cfc7c4b1bf8e121f15c0aec6b9c4c58
IT547 <antonio.giuliani@infotn.it>
2018-06-01 12:03:25 +0200

    add jenkinsfile

---
+++ jenkinsFile.groovy
@@ -0,0 +1,14 @@
+node{
+    def mvnHome = tool "Maven"
+    stage('Clone'){
+    checkout scm
+    }
+    stage('Unit tests'){
+    sh "mvn clean test"
+    }
+    stage('Integration test'){
+    echo "qui dovre lanciare i test di integrazione"
+    //sh "mvn test-compile failsafe:integration-test"
+
+    }
+}
