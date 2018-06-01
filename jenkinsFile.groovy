27ee0adbe36b740782e4d699010d0cb65faa2f0f
IT547 <antonio.giuliani@infotn.it>
2018-06-01 12:08:44 +0200

    add jenkinsfile target

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -6,6 +6,10 @@ node {
     stage('Unit tests') {
         sh "mvn clean test"
         sh "ls -l target"
+        junit'target/surefire-report//**/*.xml'
+        def antonio = "giuliani"
+        echo "ciao $antonio"
+        echo 'ciao $antonio'
     }
     stage('Integration test') {
         echo "qui dovre lanciare i test di integrazione"
